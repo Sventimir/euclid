bc4f025d2a9d4ea3f2e1ff7f60ea90c95d95b50d
Sventimir <svantimir@gmail.com>
2019-08-27 09:12:15 +0200

    Initialize SDL display and set up basic event loop.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified SDL.Init as SDL
+import SDL (($=))
+import qualified SDL
+import Linear (V4(..))
+
+
+loop :: SDL.Renderer -> [SDL.Event] ->  IO ()
+loop renderer (e : es) = case SDL.eventPayload e of
+        SDL.QuitEvent -> return ()
+        _ -> SDL.present renderer >> loop renderer es
+loop renderer [] = do
+    events <- SDL.pollEvents
+    loop renderer events
+
+initView :: IO (SDL.Window, SDL.Renderer)
+initView = do
+    win <- SDL.createWindow "Geometric constructor" $ SDL.defaultWindow {
+        SDL.windowMode = SDL.FullscreenDesktop
+    }
+    renderer <- SDL.createRenderer win (-1) SDL.defaultRenderer
+    SDL.rendererDrawColor renderer $= V4 255 255 255 255
+    SDL.clear renderer
+    SDL.present renderer
+    return (win, renderer)
+
 
 main :: IO ()
 main = do
     SDL.initialize [ SDL.InitEvents ]
+    (_, renderer) <- initView
+    loop renderer []
     SDL.quit
